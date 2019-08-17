6bebf0257d5370c8000d0b60152407297e21e46d
Charles Hong <genkihong@gmail.com>
2019-08-17 14:30:02 +0800

    Updates

---
+++ precache-manifest.82b80e5d77ba1dc8da3fc8323fa669c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60a6e39e195fd1c865826bd35c860f9",
+    "url": "/KH-traveling/index.html"
+  },
+  {
+    "revision": "86e0aff243f1447f21c5",
+    "url": "/KH-traveling/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "718a6d68264374871244",
+    "url": "/KH-traveling/static/css/main.824fc153.chunk.css"
+  },
+  {
+    "revision": "86e0aff243f1447f21c5",
+    "url": "/KH-traveling/static/js/2.9a0ca6cb.chunk.js"
+  },
+  {
+    "revision": "718a6d68264374871244",
+    "url": "/KH-traveling/static/js/main.d14eecc7.chunk.js"
+  },
+  {
+    "revision": "76a09fe75bc2a3f12171",
+    "url": "/KH-traveling/static/js/runtime~main.c2213031.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a0b695cb6525a89cf7c9e48e0cab47a8.js"
+  "/KH-traveling/precache-manifest.82b80e5d77ba1dc8da3fc8323fa669c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/KH-traveling/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
