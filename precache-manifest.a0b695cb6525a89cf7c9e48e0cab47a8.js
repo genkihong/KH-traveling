fdc683a2994cdb455279e52efc486c8847641ed7
Charles Hong <genkihong@gmail.com>
2019-08-17 13:43:49 +0800

    高雄旅遊網

---
+++ precache-manifest.a0b695cb6525a89cf7c9e48e0cab47a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf5f9bfae9fcd597cee9aababf21ae8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "053d99f732d949b67d82",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e8a96edd9ea159a2d14b",
+    "url": "/static/css/main.824fc153.chunk.css"
+  },
+  {
+    "revision": "053d99f732d949b67d82",
+    "url": "/static/js/2.13e20cbf.chunk.js"
+  },
+  {
+    "revision": "e8a96edd9ea159a2d14b",
+    "url": "/static/js/main.11135969.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
